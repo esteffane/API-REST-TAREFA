a4ae404da71f87bf5562a931c64eac1d5f3eb690
esteffane <55981920+esteffane@users.noreply.github.com>
2023-12-05 22:52:55 +0000

    primeiro

---
+++ Estefane/src/main/java/com/example/estefane/EstefaneApplication.java
@@ -0,0 +1,13 @@
+package com.example.estefane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstefaneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstefaneApplication.class, args);
+    }
+
+}
---
+++ Estefane/src/test/java/com/example/estefane/EstefaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estefane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstefaneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
