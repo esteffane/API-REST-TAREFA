73960b19ce8b1fd9e3ca5d24617319b93b78258c
esteffane <55981920+esteffane@users.noreply.github.com>
2023-12-06 01:43:31 +0000

    terceiro

---
+++ src/main/java/com/example/estefane/Repository/CurriculoRepository.java
@@ -0,0 +1,10 @@
+package com.example.estefane.Repository;
+
+import com.example.estefane.Model.Curriculo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurriculoRepository extends JpaRepository<Curriculo, Long> {
+
+
+
+}
